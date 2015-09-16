edb619dffb83b85985352492d7eaf51691dff127
Salvatore <dested@gmail.com>
2015-09-16 11:47:48 -0700

    minor tweaks to compiled code and output

--- src/services/smallDataService.js
+++ src/services/smallDataService.js
@@ -1,7 +1,28 @@
-System.register([], function(exports_1) {
+System.register(["./bigDataService", 'angular2/angular2'], function(exports_1) {
+    var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+        if (typeof Reflect === "object" && typeof Reflect.decorate === "function") return Reflect.decorate(decorators, target, key, desc);
+        switch (arguments.length) {
+            case 2: return decorators.reduceRight(function(o, d) { return (d && d(o)) || o; }, target);
+            case 3: return decorators.reduceRight(function(o, d) { return (d && d(target, key)), void 0; }, void 0);
+            case 4: return decorators.reduceRight(function(o, d) { return (d && d(target, key, o)) || o; }, desc);
+        }
+    };
+    var __metadata = (this && this.__metadata) || function (k, v) {
+        if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+    };
+    var __param = (this && this.__param) || function (paramIndex, decorator) {
+        return function (target, key) { decorator(target, key, paramIndex); }
+    };
+    var bigDataService_1, angular2_1;
     var SmallDataService;
     return {
-        setters:[],
+        setters:[
+            function (bigDataService_1_1) {
+                bigDataService_1 = bigDataService_1_1;
+            },
+            function (angular2_1_1) {
+                angular2_1 = angular2_1_1;
+            }],
         execute: function() {
             SmallDataService = (function () {
                 function SmallDataService(bigDataService) {
@@ -10,6 +31,10 @@ System.register([], function(exports_1) {
                 SmallDataService.prototype.getSmallData = function () {
                     return this.myBigDataService.getBigData().splice(0, 2);
                 };
+                SmallDataService = __decorate([
+                    __param(0, angular2_1.Inject(bigDataService_1.BigDataService)), 
+                    __metadata('design:paramtypes', [bigDataService_1.BigDataService])
+                ], SmallDataService);
                 return SmallDataService;
             })();
             exports_1("SmallDataService", SmallDataService);
